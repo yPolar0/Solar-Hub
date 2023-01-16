aa0baef69c1a220ce0fa862b2771a0a6ce773afb
yPolar7 <111068845+yPolar7@users.noreply.github.com>
2023-01-16 16:24:36 -0300

    Update TableLib.lua

--- Solar-Core/CustomFuncs/TableLib.lua
+++ Solar-Core/CustomFuncs/TableLib.lua
@@ -1,1 +1,19 @@
+local LuaEncode = loadstring(game:HttpGet(("https://raw.githubusercontent.com/regginator/LuaEncode/master/src/LuaEncode.lua")))();
 
+local TableLib = {}; do
+    function TableLib:TableToString(Table)
+        return LuaEncode(Table, {
+            PrettyPrinting = true;
+            IndentCount = 4;
+        })
+    end;
+
+    function TableLib:PrintTable(Table)
+        return print(self:TableToString(Table))
+    end;
+
+    function TableLib:CopyTable(Table)
+        return setclipboard(self:TableToString(Table))
+    end;
+end
+return TableLib;
