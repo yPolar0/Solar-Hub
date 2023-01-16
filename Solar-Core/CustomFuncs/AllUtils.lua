9e1bb7d747f8ae2528c7aaf276e2a9f8e6ced266
yPolar7 <111068845+yPolar7@users.noreply.github.com>
2023-01-16 15:36:56 -0300

    Update AllUtils.lua

--- Solar-Core/CustomFuncs/AllUtils.lua
+++ Solar-Core/CustomFuncs/AllUtils.lua
@@ -1,1 +1,8 @@
-
+local Utils = {
+    Pathfinding = loadstring(game:HttpGet(("https://raw.githubusercontent.com/yPolar7/Solar-Hub/main/Solar-Core/CustomFuncs/PathfindingLib.lua")))();
+    Network = loadstring(game:HttpGet(("https://raw.githubusercontent.com/yPolar7/Solar-Hub/main/Solar-Core/CustomFuncs/Network.lua")))();
+    Webhook = loadstring(game:HttpGet(("https://raw.githubusercontent.com/yPolar7/Solar-Hub/main/Solar-Core/CustomFuncs/WebhookLib.lua")))();
+    Suffix = loadstring(game:HttpGet(("https://raw.githubusercontent.com/yPolar7/Solar-Hub/main/Solar-Core/CustomFuncs/SuffixLib.lua")))();
+    Table = loadstring(game:HttpGet(("https://raw.githubusercontent.com/yPolar7/Solar-Hub/main/Solar-Core/CustomFuncs/TableLib.lua")))();
+};
+return Utils
