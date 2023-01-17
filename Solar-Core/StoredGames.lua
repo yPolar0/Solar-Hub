62b926a8351c26cadbb95ae973a614896b3f74d4
yPolar7 <111068845+yPolar7@users.noreply.github.com>
2023-01-17 15:05:16 -0300

    Update StoredGames.lua

--- Solar-Core/StoredGames.lua
+++ Solar-Core/StoredGames.lua
@@ -4,5 +4,6 @@ return {
     ["1362482151"] = "Dragon%20Ball%20Rage";
     ["3371469539"] = "Dragon%20Ball%20Rage";
     ["3336119605"] = "Dragon%20Ball%20Rage";
+    ["6461766546"] = "A%20Hero%20Destiny";
     ["1537690962"] = "Bee%20Swarm%20Simulator";
 }
