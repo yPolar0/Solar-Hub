0cc679d3f035c60d2062a75b80f76356c741eb91
yPolar7 <111068845+yPolar7@users.noreply.github.com>
2023-02-16 12:53:01 -0300

    positions

--- Solar-Core/CustomFuncs/Linoria.lua
+++ Solar-Core/CustomFuncs/Linoria.lua
@@ -2430,7 +2430,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 100, 0, -25);
+        Position = UDim2.new(0, 100, 0, -30);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
